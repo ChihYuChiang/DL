96c4a61dd5ed81a55f626d97b2df03012ca8cbfa
ChihYuChiang <ckccyu@gmail.com>
2018-02-24 19:34:31 -0600

    Base: convolnet

---
+++ bin/c4_1m_Convolution_Model.py
@@ -0,0 +1,10 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+np.random.seed(1)
+
